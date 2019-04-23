1ba0cc22a67a0d205a37cb87c3f0b68d4d2d3ac9
Peter L. Berghold <pberghold@tenna.com>
2019-04-23 19:34:41 +0000

    root not rooot

--- manifests/machine.pp
+++ manifests/machine.pp
@@ -8,7 +8,7 @@ class docker::machine {
   $base='https://github.com/docker/machine/releases/download/v0.16.0'
   $hw = "$::os['hardware]"
   $url="${base}/docker-macine/${::kernel}-${hw}"
-  $dest='/usr/local/bin docker-machine'
+  $dest='/usr/local/bin/docker-machine'
   $cmd = "curl -L ${url} > ${dest}"
 
   exec { $cmd:
