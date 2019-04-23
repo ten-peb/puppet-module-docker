24c35e3c4a730750d366ff4bba54ec8852527773
Peter L. Berghold <pberghold@tenna.com>
2019-04-23 19:07:17 +0000

    added docker::machine

---
+++ manifests/machine.pp
@@ -0,0 +1,26 @@
+# @summary A short summary of the purpose of this class
+#
+# Installs docker-machine on a host 
+#
+# @example
+#   include docker::machine
+class docker::machine {
+  $base='https://github.com/docker/machine/releases/download/v0.16.0'
+  $hw = "$::os['hardware]"
+  $url="${base}/docker-macine/${::kernel}-${hw}"
+  $dest='/usr/local/bin docker-machine'
+  $cmd = "curl -L ${url} > ${dest}"
+
+  exec { $cmd:
+    path   => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin',
+    onlyif => "[ ! -f ${dest}"
+  }
+
+  file { $dest:
+    ensure => present,
+    owner  => 'rooot',
+    group  => 'root',
+    mode   =>  '0755',
+    require => Exec[$cmd]
+  }
+}
